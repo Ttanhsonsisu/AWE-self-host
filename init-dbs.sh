1f1654d33bcd29f838c7e4795371d3c34ac2fcf7
AnhTT <ttanhsonsisu@gmail.com>
2026-05-09 14:50:59 +0700

    first commit

---
+++ init-dbs.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+set -e
+
+# Chạy lệnh SQL bằng quyền của superuser
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE awe_db;
+    CREATE DATABASE keycloak_db;
+    GRANT ALL PRIVILEGES ON DATABASE awe_db TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO $POSTGRES_USER;
+EOSQL
